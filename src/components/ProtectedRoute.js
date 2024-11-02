7c986022cdbe3ae84b810492fcfdc74bb86d0a78
mingrui <t3cami00@students.oamk.fi>
2024-11-02 07:45:13 +0200

    todo_7

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useUser } from "../context/useUser";
 
 export default function ProtectedRoute() {
-  const user = useUser();
+  const { user } = useUser();
 
   if (!user || !user.token) {
     return <Navigate to="/signin" />;
